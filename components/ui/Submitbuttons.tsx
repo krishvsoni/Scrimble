a71fabee3810d51b26726d3e108ae728d770fb8b
= <=>
2024-02-29 23:18:58 +0530

    themes added

---
+++ components/ui/Submitbuttons.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Loader2, Trash } from "lucide-react";
+import { useFormStatus } from "react-dom";
+
+export function SubmitButton() {
+  const { pending } = useFormStatus();
+  return (
+    <>
+      {pending ? (
+        <Button disabled className="w-fit">
+          <Loader2 className="mr-2 w-4 h-4 animate-spin" /> Please Wait
+        </Button>
+      ) : (
+        <Button className="w-fit" type="submit">
+          Save Now
+        </Button>
+      )}
+    </>
+  );
+}
+
+
+
+
+
