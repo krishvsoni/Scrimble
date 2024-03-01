098f35bfb153cb819094bd31a240f977f713a893
krishvsoni <sonikrish994@gmail.com>
2024-03-02 00:17:37 +0530

    Payment Gateway Integration

---
+++ prisma/migrations/20240301175015_schema_validate/migration.sql
@@ -0,0 +1,41 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "colorScheme" SET DEFAULT 'theme-violet';
+
+-- CreateTable
+CREATE TABLE "Subscription" (
+    "stripeSubscriptionId" TEXT NOT NULL,
+    "invterval" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "planId" TEXT NOT NULL,
+    "currentPeriodStart" INTEGER NOT NULL,
+    "currentPeriodEnd" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Subscription_pkey" PRIMARY KEY ("stripeSubscriptionId")
+);
+
+-- CreateTable
+CREATE TABLE "Note" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT,
+
+    CONSTRAINT "Note_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Subscription_stripeSubscriptionId_key" ON "Subscription"("stripeSubscriptionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Subscription_userId_key" ON "Subscription"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Note" ADD CONSTRAINT "Note_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
