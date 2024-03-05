2c959352b5f0cf63cb8bfe238707139cdcc4e387
krishvsoni <sonikrish994@gmail.com>
2024-03-05 20:54:37 +0530

    dashboard responsive

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -60,7 +60,7 @@ export default async function DashboardPage() {
     <div className="grid items-start gap-y-8">
       <div className="flex items-center justify-between px-2">
         <div className="grid gap-1">
-          <h1 className="text-3xl md:text-4xl">Your Notes</h1>
+          <h1 className="text-2xl md:text-2xl">Your Notes</h1>
           {/* <p className="text-lg text-muted-foreground">
             Your Notes Listed
           </p> */}
